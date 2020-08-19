3afba2ceb16b3e7f375a865e1613cee4716a6544
Luis Glaser <Luis.Glaser@stud.uni-frankfurt.de>
2020-08-19 13:58:42 +0200

    simplified pagehandler

--- src/main/java/org/acoli/glaser/metadata/pdf/PageHandler.java
+++ src/main/java/org/acoli/glaser/metadata/pdf/PageHandler.java
@@ -3,28 +3,52 @@ package org.acoli.glaser.metadata.pdf;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class PageHandler {
+    @Deprecated
     List<MetadataFromHTML> htmlSources;
+    @Deprecated
     List<MetadataFromPDF> pdfSources;
     List<Metadata> mds;
+    List<MetadataSourceHandler> sources;
 
     public PageHandler() {
         htmlSources = new ArrayList<>();
         pdfSources = new ArrayList<>();
         mds = new ArrayList<>();
+        sources = new ArrayList<>();
+    }
+    public PageHandler(List<MetadataSourceHandler> initialSources) {
+        this.sources = initialSources;
+    }
+
+    /**
+     * TODO: We could potentially get into an infinite loop because we don't remember where we were already
+     * @param handler
+     */
+    void unwindFinishedAndSuccessfulHandler(MetadataSourceHandler handler) {
+        assert handler.finished(); // Maybe this is bad practice?
+        if (handler.foundOtherSourcesThatRequireHandling()) {
+            List<MetadataSourceHandler> newSources = handler.getHandlersForOtherSources();
+            System.err.println(handler+" found "+newSources.size()+" new sources, adding them.");
+            sources.addAll(handler.getHandlersForOtherSources());
+        }
+        mds.addAll(handler.getMetadata());
     }
     public void run() {
-        while (!htmlSources.isEmpty() || !pdfSources.isEmpty()) {
-            System.err.println(htmlSources.size()+" html sources and "+pdfSources.size()+" pdf sources left to extract..");
-            if (!htmlSources.isEmpty()) {
-                MetadataFromHTML next = htmlSources.remove(0);
-                next.addSummaryPageToPool(this.mds);
-                if (next.pageContainsPDFs()) {
-                    for (String urlToPDF : next.getPDFsOnPage()) {
-                        System.err.println("WOULD ADD "+urlToPDF+" TO PDF SEED NOW.");
-                    }
-                }
+        while (!sources.isEmpty()) {
+            System.err.println(sources.size()+" sources left to extract..");
+            MetadataSourceHandler source = sources.remove(0);
+            System.err.println("Handling "+source+" now..");
+            source.run();
+            if (source.finished()) {
+                if (source.success())
+                    unwindFinishedAndSuccessfulHandler(source);
+                else
+                    System.err.println(source+" unsuccessful");
             }
+            if (source.finished() && source.failed())
+                System.err.println("FAILED "+source);
         }
     }
 }
