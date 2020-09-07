1e77e107c2053be7a0d63f5870a0c8ab55709de3
Luis G <lglaser@cs.uni-frankfurt.de>
2020-09-07 14:07:48 +0200

    finished merging

--- src/main/java/org/acoli/glaser/metadata/pdf/MetadataMerger.java
+++ src/main/java/org/acoli/glaser/metadata/pdf/MetadataMerger.java
@@ -1,5 +1,7 @@
 package org.acoli.glaser.metadata.pdf;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class MetadataMerger {
@@ -35,8 +37,49 @@ public class MetadataMerger {
         return -1;
     }
     static public List<String> mergeList(List<String> a, List<String> b) {
-        a.addAll(b);
+        if (a.size() == 0) {
+            return b;
+        } else if (b.size() == 0) {
+            return a;
+        }
+        else {
+            a.retainAll(b);
+        }
         return a;
+    }
+
+    static public boolean areMergable(Metadata a, Metadata b) {
+        return a.title.equals(b.title);
+    }
+
+    static public List<Metadata> mergeMetadata(List<Metadata> mds){
+        List<Metadata> uniqMds = removeDuplicates(mds);
+        if (uniqMds.size()==1) {
+            return uniqMds;
+        }
+        List<Metadata> mergedMetadata = new ArrayList<>();
+        for (int i = 0; i < uniqMds.size()-1; i++) {
+            Metadata a = uniqMds.get(i);
+            Metadata b = uniqMds.get(i+1);
+            if (areMergable(a, b)) {
+                mergedMetadata.add(mergeMetadata(a, b));
+            } else {
+                mergedMetadata.add(a);
+                if (i == uniqMds.size()-2) {
+                    mergedMetadata.add(b);
+                }
+            }
+        }
+        return mergedMetadata;
+    }
+
+    /**
+     * This SHOULD have little impact on the performance.
+     * @param mds
+     * @return
+     */
+    static public List<Metadata> removeDuplicates(List<Metadata> mds){
+        return new ArrayList<>(new HashSet<>(mds));
     }
     static public Metadata mergeMetadata(Metadata a, Metadata b){
         Metadata c = new Metadata();
